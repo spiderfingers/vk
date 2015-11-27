4927fb64d38786925b3b2f82e7a11c0423755a83
Andrey Slepuhin <a_slepuhin@wargaming.net>
2015-11-27 18:42:25 +0300

    initial comit

---
+++ snob.py
@@ -0,0 +1,19 @@
+import vkontakte
+import vk_auth
+import getpass
+
+snob_parser_id = '5161360'
+snob_group_id = '-101947002'
+my_login = 'interstellarkid@gmail.com'
+my_password = getpass.getpass()
+
+access_token = vk_auth.auth(my_login, my_password, snob_parser_id, "audio,status")[0]
+
+vk = vkontakte.API(token=access_token)
+all_tunes_in_public = vk.audio.get(owner_id=snob_group_id)
+
+# for tune in all_tunes_in_public:
+#     for tune_field in tune
+
+
+

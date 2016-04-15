0157f5309ae3e2bc7fd2888bde059beececc1f77
spiderfingers <interstellarkid@gmail.com>
2016-04-15 19:19:44 +0400

    Update snob.py

--- snob.py
+++ snob.py
@@ -4,7 +4,7 @@ import getpass
 
 snob_parser_id = '5161360'
 snob_group_id = '-101947002'
-my_login = 'interstellarkid@gmail.com'
+my_login = ''
 my_password = getpass.getpass()
 
 access_token = vk_auth.auth(my_login, my_password, snob_parser_id, "audio,status")[0]
